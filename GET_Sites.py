9c56fbf07e655014d107999179682f769890eb3c
Your Name <mali@artofrf.com>
2026-02-27 12:13:42 -0500

    Updating files

--- GET_Sites.py
+++ GET_Sites.py
@@ -1,13 +1,16 @@
 import http.client
+import pprint as pp
+from pprint import pprint
 
-input = input("Enter the X-API-Key: ")
+api_key = input("Enter the X-API-Key: ")
 conn = http.client.HTTPSConnection("api.ui.com")
 payload = ''
 headers = {
   'Accept': 'application/json',
-  'X-API-Key': '<X-API-Key>'
+  'X-API-Key': api_key
 }
 conn.request("GET", "/v1/sites?pageSize=10&nextToken=602232A870250000000006C514FF00000000073DD8DB000000006369FDA2:1467082514", payload, headers)
 res = conn.getresponse()
 data = res.read()
-print(data.decode("utf-8"))
+pp(data)
+#pp(data.decode("utf-8"))
