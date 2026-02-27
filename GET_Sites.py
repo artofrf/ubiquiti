5dd0f8ee17644672be3b0e72b78a7ad5802dfc9f
Your Name <mali@artofrf.com>
2026-02-27 17:14:04 -0500

    breaking things

--- GET_Sites.py
+++ GET_Sites.py
@@ -4,13 +4,3 @@ import json
 
 api_key = input("Enter the X-API-Key: ")
 conn = http.client.HTTPSConnection("api.ui.com")
-payload = ''
-headers = {
-  'Accept': 'application/json',
-  'X-API-Key': api_key
-}
-conn.request("GET", "/v1/sites?pageSize=10&nextToken=602232A870250000000006C514FF00000000073DD8DB000000006369FDA2:1467082514", payload, headers)
-res = conn.getresponse()
-data = res.read()
-data_json = json.loads(data.decode("utf-8"))
-pp(data_json)
--- GET_wifi.py
+++ GET_wifi.py
@@ -9,7 +9,3 @@ headers = {
   'Accept': 'application/json',
   'X-API-Key': api_key
 }
-conn.request("GET", f"/v1/connector/consoles/{consoleId}/proxy/network/integration/v1/sites/{siteId}/wifi/broadcasts?offset=%7Boffset%7D&limit=%7Blimit%7D&filter=%7Bfilter%7D", payload, headers)
-res = conn.getresponse()
-data = res.read()
-print(data.decode("utf-8"))
