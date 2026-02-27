ce7e1bb9f182cd627e8b9fc1091d61b9f79e6714
Your Name <mali@artofrf.com>
2026-02-27 12:42:34 -0500

    Revert "Adding a comment towards the end"
    This reverts commit 9954fbc33c7b214c889d15716ffef43fc35901ce.

--- GET_Sites.py
+++ GET_Sites.py
@@ -11,3 +11,5 @@ headers = {
 conn.request("GET", "/v1/sites?pageSize=10&nextToken=602232A870250000000006C514FF00000000073DD8DB000000006369FDA2:1467082514", payload, headers)
 res = conn.getresponse()
 data = res.read()
+print(data.decode("utf-8"))
+// The above code is a Python script that makes an HTTP GET request to the Ubiquiti API to retrieve a list of sites. It prompts the user to enter their API key, sets up the connection and headers, and then sends the request. The response is read and printed in JSON format.
